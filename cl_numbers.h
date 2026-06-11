bf467e5a052bb5c83d826a30a33f583489866a79
Claudio Audisio <claudio.audisio@gmail.com>
2026-06-11 20:38:10 +0200

    added some new function in numbers.h

--- cl_numbers.h
+++ cl_numbers.h
@@ -6,4 +6,17 @@ namespace cl {
 	static bool isWholeNumber(const double x) {
 		return fabs(x - round(x)) < (x / 1e6);
 	}
+
+	static bool isEqual(const float value, const float target, const float epsilon) {
+		return fabs(value - target) < epsilon;
+	}
+
+	static bool isBetween(const int value, const int lb, const int ub) {
+		return value >= lb && value <= ub;
+	}
+
+	static bool isBetween(const float value, const float lb, const float ub) {
+		return value >= lb && value <= ub;
+	}
+
 }
